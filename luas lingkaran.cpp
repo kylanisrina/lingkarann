3504b1e1755fe5a4e3ba4680efbef92b3882fbfc
KYLA NISRINA ANGGRAHINI <88182323+kylanisrina@users.noreply.github.com>
2022-09-09 15:21:13 +0700

    Add files via upload

---
+++ luas lingkaran.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	system("color A");
+	float r, luas;
+ 
+    cout<<"Jari-jari lingkaran: ";
+   cin>>r;
+   luas = 3.12 * r * r;
+ 
+   cout << "Luas lingkaran = "<< luas << endl;
+	
+	return 0;
+}
